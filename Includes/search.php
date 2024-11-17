27e125389ec09f0dd6ef4da1f1b574c3c3d87c2f
Andy <66146370+andosgee@users.noreply.github.com>
2024-11-17 22:09:04 +1300

    Sample Data

--- Includes/search.php
+++ Includes/search.php
@@ -1,4 +1,14 @@
 <div class="search">
+    <?php
+    include_once 'Classes/Database.php';
+    $db = new Database();
+    $conn = $db->connect();
+$queryID = "SELECT * FROM event WHERE Publish = 1 ORDER BY event_id DESC LIMIT 1";
+$stmt = $conn->prepare($queryID);
+$stmt->execute();
+$event = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    ?>
             <p><a href="search.php">Click Here to Search for Students, Pathways, Projects, or Events.</a></p>
-            <p><a href="event.php">Click Here to go to the Most Recent Event.</a></p>
+            <p><a href="event.php?event_ID=<?php echo $event['event_id'];?>">Click Here to go to the Most Recent Event.</a></p>
         </div>
